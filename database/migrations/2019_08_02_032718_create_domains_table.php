3cb399335a3fee8340e66c50613fa31f8f535c5d
Arkadiy Stepanov <arkadiy93@hotmail.com>
2019-10-13 20:47:50 +0200

    db refactoring

--- database/migrations/2019_08_02_032718_create_domains_table.php
+++ database/migrations/2019_08_02_032718_create_domains_table.php
@@ -14,7 +14,7 @@ class CreateDomainsTable extends Migration
     public function up()
     {
         Schema::create('domains', function (Blueprint $table) {
-            $table->bigIncrements('id')->nullable();
+            $table->bigIncrements('id');
             $table->string('name')->nullable();
             $table->integer('status_code')->nullable();
             $table->longText('body')->nullable();
