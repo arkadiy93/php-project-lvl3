d450ff3ad2c32db88509a1eec7860c0b65fa752a
Arkadiy Stepanov <arkadiy93@hotmail.com>
2019-10-13 18:48:00 +0200

    add di container

---
+++ app/Http/Controllers/DomainsController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Domains;
+use DiDom\Document;
+use GuzzleHttp\Client;
+
+class DomainsController extends Controller
+{
+    protected $client;
+
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    public function initialize()
+    {
+        return view('index');
+    }
+
+    public function saveDomain(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'url' => 'required|url',
+        ]);
+    
+        if ($validator->fails()) {
+            $urlError = $validator->errors()->first('url');
+            $errors = ['url' => $urlError];
+            return view('index', ['errors' => $errors]);
+        }
+        
+        $url = $request->get('url');
+        $response = $this->client->get($url);
+    
+        $body = $response->getBody()->getContents();
+        $code = $response->getStatusCode();
+        $contentLengthHeader = $response->getHeader('Content-Length')[0] ?? 'no data';
+        
+        $domains = new Domains();
+        $domains->name = $url;
+        $domains->body = $body;
+        
+        $document = new Document($body);
+        $domains->heading = $document->has('h1') ? $document->find('h1')[0]->text() : 'no data';
+        $domains->description = $document->find('meta[name=description]::attr(content)')[0] ?? 'no data';
+        $domains->keywords = $document->find('meta[name=keywords]::attr(content)')[0] ?? 'no data';
+        
+        $domains->status_code = $code;
+        $domains->content_length = $contentLengthHeader;
+        $domains->save();
+    
+        $id = $domains->id;
+        return redirect()->route('domains.show', ['id' => $id]);
+    }
+
+    public function showDomain($id)
+    {
+        $domain = Domains::find($id);
+        return view('list', ['domains' => [$domain]]);
+    }
+
+    public function getDomainList()
+    {
+        $domains = Domains::paginate(5);
+        return view('list', ['domains' => $domains]);
+    }
+}
--- tests/AppTest.php
+++ tests/AppTest.php
@@ -4,10 +4,40 @@ namespace Tests;
 
 use Laravel\Lumen\Testing\DatabaseMigrations;
 use Laravel\Lumen\Testing\DatabaseTransactions;
+use GuzzleHttp\Handler\MockHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Psr7\Response;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Exception\RequestException;
+use App\Http\Controllers\DomainsController;
+
+
+// Create a mock and queue two responses.
+$mock = new MockHandler([
+    new Response(200, ['X-Foo' => 'Bar']),
+    new Response(202, ['Content-Length' => 0]),
+    new RequestException("Error Communicating with Server", new Request('GET', 'test'))
+]);
+
 
 class AppTest extends TestCase
 {
     use DatabaseTransactions;
+
+    public function setUp() :void
+    {
+        parent::setUp();
+        $this->app->bind(DomainsController::class, function () {
+            $response_200 = json_encode(array("status" => "successful"));
+        
+            $mock = new MockHandler([new Response(200, [], $response_200)]);
+        
+            $handler = HandlerStack::create($mock);
+        
+            return new DomainsController(new \GuzzleHttp\Client(['handler' => $handler]));
+        });
+    }
+
     /**
      * A basic test example.
      *
